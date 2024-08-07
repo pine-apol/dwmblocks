fb89351ce7cd571a0266292fec391e8c6cb3ffa7
pine-apol <pine2cey@protonmail.com>
2024-08-08 03:10:58 +0800

    updated status

--- blocks.def.h
+++ blocks.def.h
@@ -1,7 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "/home/pine/.scripts/status.sh",  60, 0},
+	{"", "/home/pine/.scripts/status.sh",  1, 0},
 	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
 
 };
